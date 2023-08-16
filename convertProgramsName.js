188aa95594d9265560c27bd93debadfdd3cf84ac
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-08-16 19:32:49 -0300

    Finalizando função para pegar dado dos programas.

--- convertProgramsName.js
+++ convertProgramsName.js
@@ -1,26 +1,16 @@
 function convertProgramsName() {
 
-    const planilha = SpreadsheetApp.getActiveSpreadsheet();
-    const programaDestinoNome = planilha.getSheetByName("LayoutHeader").getRange("C4").getValue;
-    const programaDestinoTamanho = planilha.getSheetByName("LayoutHeader").getRange("B4").getValue;
-    const originProgramNome = planilha.getSheetByName("LayoutHeader").getRange("C5").getValue;
-    const originProgramTamanho = planilha.getSheetByName("LayoutHeader").getRange("B5").getValue;
+    const sheet = SpreadsheetApp.getActiveSpreadsheet();
+    const layoutHeader = sheet.getSheetByName("LayoutHeader");
+  
+    const targetProgramName = layoutHeader.getRange("C4").getValue();
+    const targetProgramNameLength = layoutHeader.getRange("B4").getValue();
+    const sourceProgramName = layoutHeader.getRange("C5").getValue();
+    const sourceProgramNameLength = layoutHeader.getRange("B5").getValue();
+  
+    let targetProgram = sheet.getSheetByName("Conversao").getRange("C1");
+    let sourceProgram = sheet.getSheetByName("Conversao").getRange("D1");
 
-    let programaDeDestino = planilha.getSheetByName("Conversao").getRange("C1");
-    let programaDeOrigem = planilha.getSheetByName("Conversao").getRange("D1");
-
-    programaDeDestino.setValue(convertLengthWithSpace(programaDestinoNome, programaDestinoTamanho));    
-    programaDeOrigem.setValue(convertLengthWithSpace(originProgramNome, originProgramTamanho));    
-
-    function convertLengthWithSpace(celula, tamanhoEsperado){
-
-        const tamanhoCelula = celula.length;
-        const complementoCelula = ' ';
-        let resultado = '';
-        
-        for (i = tamanhoCelula; i < tamanhoEsperado; i++) {
-            resultado =  celula + complementoCelula;
-        }
-        return resultado;
-    }
+    targetProgram.setValue(convertLengthWithSpace(targetProgramName, targetProgramNameLength));
+    sourceProgram.setValue(convertLengthWithSpace(sourceProgramName, sourceProgramNameLength));
 }
