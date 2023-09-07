9a4f3066420a99891f61f2153b5c4afaa4afed11
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-09-07 11:23:25 -0300

    Ajustes na nomeclatura

--- CreateReleases/buscarEmpresa.js
+++ CreateReleases/buscarEmpresa.js
@@ -1,8 +1,6 @@
-function buscarEmpresa() {
-
-  const planilha = SpreadsheetApp.getActiveSpreadsheet();
+function buscarEmpresa(planilha) {
   
-  const celula = planilha.getSheetByName("Lancamentos").getRange("A1:A2").getValue();
+  const empresaDeseja = planilha.getSheetByName("Lancamentos").getRange("A1:A2").getValue();
 
   const tamanhoEsperado = planilha.getSheetByName("LayoutHeader").getRange("B6").getValue();
 
@@ -10,7 +8,7 @@ function buscarEmpresa() {
   let listaNomesEmpresas = paginaEmpresas.getRange("B:B").getValues();
   let listaCodigosEmpresas = paginaEmpresas.getRange("A:A").getValues();
 
-  let resultado = buscarCodigoEmpresa(celula, listaNomesEmpresas, listaCodigosEmpresas);
+  let resultado = buscarCodigoEmpresa(empresaDeseja, listaNomesEmpresas, listaCodigosEmpresas);
 
   if (resultado.length < tamanhoEsperado) {
 
@@ -22,11 +20,11 @@ function buscarEmpresa() {
 
   return resultado;
 
-  function buscarCodigoEmpresa(celula, listaNomesEmpresas, listaCodigosEmpresas){
+  function buscarCodigoEmpresa(empresaDeseja, listaNomesEmpresas, listaCodigosEmpresas){
 
     for (let i = 0; i < listCompanyName.length; i++) {
 
-      if (celula == listaNomesEmpresas[i]) {
+      if (empresaDeseja == listaNomesEmpresas[i]) {
         return listaCodigosEmpresas[i];
       }
     }
