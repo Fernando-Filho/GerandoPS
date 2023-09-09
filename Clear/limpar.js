6881d7eb6ac2a5fc258b4df0ba6c1668cfb4d7d1
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-09-09 13:53:34 -0300

    Ajuste na função clear

--- Clear/limpar.js
+++ Clear/limpar.js
@@ -1,19 +1,6 @@
 function limpar() {
-    const planilha = SpreadsheetApp.getActiveSpreadsheet();
-    limparLancamentos(planilha);
-    limparConversao(planilha);
-    limparLayoutPS(planilha);
-}
-
-function limparLancamentos(planilha) {
-planilha.getSheetByName("Lancamentos").getRange("B1:AA2").clear();
-planilha.getSheetByName("Lancamentos").getRange("3:1000").clear();
-}
-
-function limparConversao(planilha) {
-planilha.getSheetByName("Conversao").getRange("1:1000").clear();
-}
-
-function limparLayoutPS(planilha) {
-planilha.getSheetByName("LayoutPS").getRange("1:1000").clear();
+    pagina("Lancamentos").getRange("B1:AA2").clear();
+    pagina("Lancamentos").getRange("3:1000").clear();
+    pagina("Conversao").getRange("1:1000").clear();
+    pagina("LayoutPS").getRange("1:1000").clear();
 }
