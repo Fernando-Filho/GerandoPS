60b4b6048f07511164c76e0efee7a83497dfd9a3
Fernando Filho <fernando.fs.filhorn@gmail.com>
2023-09-14 21:30:51 -0300

    melhoria no código.

--- CreateReleases/buscarEmpregado.js
+++ CreateReleases/buscarEmpregado.js
@@ -1,14 +1,13 @@
 function buscarEmpregado(celula) {
 
   const ultimaLinha = pagina("Empregados").getLastRow();
-  const listaCodigosEmprgados = pagina("Empregados").getRange(1, 1, ultimaLinha, 1).getValues();
-  const listaNomesEmprgados = pagina("Empregados").getRange(1, 2, ultimaLinha, 1).getValues();
   const matrizEmpregado = pagina("Empregados").getRange(1,1,ultimaLinha,3).getValues();
+  const empresaDeseja = buscarEmpresa();
 
-  for (i = 0; i <= matrizEmpregado.length; i++) {
+  for (let i = 0; i <= matrizEmpregado.length; i++) {
 
-    if (celula == listaNomesEmprgados[i][1] && buscarEmpresa() == listaNomesEmprgados[i][2]) {
-        return listaCodigosEmprgados[i][0];
+    if (celula == matrizEmpregado[i][1] && empresaDeseja == matrizEmpregado[i][2]) {
+      return  matrizEmpregado[i][0];
     }
   }
   return 'O empregado não foi encontrado, por favor atualize sua lista de empregados!';
